e8f495905da8b9749cbeecc832513bdec97c623d
Dhruv K. Aggarwal <165761845+Dhruv-KAggarwal@users.noreply.github.com>
2025-02-27 15:04:03 +0530

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -19,7 +19,7 @@ def get_gemini_response(question):
 
 st.set_page_config(page_title="Q&A Demo")
 
-st.header("SOLOS")
+st.header("Enter your LAW related queries here (eg: any particular article")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
